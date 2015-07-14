4c2ca6d6b313f42f1ca8dc5761c01c4b62b7962f
Pooyan Khosravi <pekhee@gmail.com>
2015-07-15 00:34:37 +0800

    initial went - broken

---
+++ src/went.rb
@@ -0,0 +1,38 @@
+class Parser
+  def self.sum(arr)
+    sum = 0
+    for i in arr
+      sum += i.to_i
+    end
+    puts sum
+  end
+
+  def self.minus(arr)
+    sum = 0
+    for i in arr
+     sum -= i.to_i
+    end
+    puts sum
+  end
+
+end
+
+class REPL
+  def self.exec
+    n = 0
+    while true
+       print "WENT [#{n}] - > "
+       cmd = gets.chomp
+       if cmd.split(' ').include? "+"
+         Parser.sum cmd.split(' ')
+       elsif cmd.split(' ').include? "-"
+         Parser.minus cmd.split(' ')
+       else
+         puts ' "#{cmd}" is not a WENT command'
+       end
+       n += 1
+    end
+  end
+end
+
+REPL.exec
